7c5e92c345bfe436b54996bfc141015fb5fde795
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-03-31 18:08:31 +0530

    Update kingibrahim.js

--- commandes/kingibrahim.js
+++ commandes/kingibrahim.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "sahan", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'KINGIBRAHIM-HD-PHOTO \n\n ' + "*ⁱᵇʳᵃʰⁱᵐ-ᵗᵉᶜʰ.*";
-    let d = ' #KING'S';
+    let z = 'MASTERMIND-HD-PHOTO \n\n ' + "*ⁱᵇʳᵃʰⁱᵐ-ᵗᵉᶜʰ.*";
+    let d = ' #MASTER'S';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/eb92cca50f07bb52f030d.jpg';
+    var img = 'https://telegra.ph/file/3b5e3a9b55b5ec0df4bf8.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
