00e6ded1f3ff42ab9675f8c53ca00da1bfbb5ed3
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-04-23 08:53:04 -0400

    💀🥷🏻MASTER-MD🥷🏻💀

--- set.js
+++ set.js
@@ -14,6 +14,7 @@
 //                                                                                                                 //
 //                                                                                                                 //
 //═════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+
 const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
 if (fs.existsSync('set.env'))
